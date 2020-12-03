8e8c9aa7225ffcd3747129f4c8662b1837027119
zixiCat <zixicat@qq.com>
2020-12-03 11:37:25 +0800

    chore: update setting

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,16 @@
-/**
- * The following are the devDependencies needed,
- * you can also install them directly by using:
- * yarn add --dev rollup rollup-plugin-sass rollup-plugin-typescript2
- * The devDependencies:
- * rollup
- * rollup-plugin-sass
- * rollup-plugin-typescript2
- * */
-import sass from 'rollup-plugin-sass';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
   input: 'src/index.tsx',
   output: [
     {
-      dir: 'dist',
+      dir: 'lib',
       format: 'cjs',
       exports: 'named',
       sourcemap: false,
       strict: false,
     },
   ],
-  plugins: [sass({ insert: false }), typescript()],
+  plugins: [typescript()],
   external: ['react', 'react-dom'],
 };
